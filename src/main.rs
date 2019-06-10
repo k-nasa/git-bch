0fb5124460bbf1af6381f3883ab2d06e819121f4
k-nasa <htilcs1115@gmail.com>
2019-06-10 20:42:29 +0900

    feat: Sort branches

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,13 @@ fn main() {
         .expect("Failed run git-branch command");
 
     let output_branch_raw = git_branch_process.wait_with_output().unwrap().stdout;
-    let output_branch: Vec<String> = String::from_utf8(output_branch_raw)
+    let mut output_branch: Vec<String> = String::from_utf8(output_branch_raw)
         .unwrap()
         .split('\n')
         .map(|branch| branch.trim().to_string())
         .filter(|branch| !branch.is_empty())
         .collect();
+    output_branch.sort();
 
     println!("{:?}", output_branch);
 }
