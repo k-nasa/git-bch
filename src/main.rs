0543b044f4b24b360fd4a94e1920b55dbd4b3442
k-nasa <htilcs1115@gmail.com>
2019-06-10 10:00:09 +0900

    feat: Get git-branch information

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::process;
+
 fn main() {
-    println!("Hello, world!");
+    let git_branch_process = process::Command::new("git")
+        .arg("branch")
+        .stdout(process::Stdio::piped())
+        .spawn()
+        .expect("Failed run git-branch command");
+
+    let output_branch_raw = git_branch_process.wait_with_output().unwrap().stdout;
+    let output_branch: Vec<String> = String::from_utf8(output_branch_raw)
+        .unwrap()
+        .split('\n')
+        .map(|branch| branch.trim().to_string())
+        .filter(|branch| !branch.is_empty())
+        .collect();
+
+    println!("{:?}", output_branch);
 }
